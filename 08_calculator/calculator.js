1568de3f8bbcb748bb0d985127133fa07d85406a
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-19 20:55:59 -0500

    Complete 08_calculator
    Write all calculator functions. All tests pass.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((total, currentNum) => total + currentNum, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(num1, num2) {
+  if (typeof num1 === 'number' && typeof num2 === 'number') {
+    return num1 * num2;
+  }
+  else {
+    let total = 1;
+    if (typeof num1 === 'object') {
+      num1.forEach(number => total *= number);
+    }
+    else if (typeof num2 === 'object') {
+      num2.forEach(number => total *= number);
+    }
+    return total;
+  }
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  }
+  let total = 1;
+  for (let i = num; i >= 1; i--) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
