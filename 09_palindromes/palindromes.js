29eb46f5effa6c59ee9fafed9b6eb3fe5c7583c7
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-19 22:22:15 -0500

    Complete 09_palindromes
    Write function to determine if string is a palindrome.
    All tests pass.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
-};
+const palindromes = function (str) {
+    let result = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let reverse = result.split('').reverse().join('');
+    return result === reverse;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
