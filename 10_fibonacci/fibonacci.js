cb24cb739091abb6964a3a9f9348014cfa003970
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-21 14:31:58 -0500

    Complete 10_fibonacci
    Write function to generate fibonacci sequence.
    All tests pass.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    const fibNum = parseInt(fib);
+    if (fibNum < 0) {
+        return 'OOPS';
+    }
+    let firstNum = 0;
+    let nextNum = 1;
+    let lastNum;
+    for (let i = 1; i <= fibNum; i++) {
+        lastNum = firstNum + nextNum;
+        firstNum = nextNum;
+        nextNum = lastNum;
+    }
 
+    return firstNum;
 };
 
 // Do not edit below this line
