8c882ef799f4a2c0eff62342372585c9450f3437
jzanderson09 <91779418+jzanderson09@users.noreply.github.com>
2024-04-21 15:39:39 -0500

    Complete 12_findTheOldest
    Write functions to extract age and compare using reduce.
    All tests pass.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((oldestPerson, currentPerson) => {
+        const oldestAge = getAge(oldestPerson);
+        const currentAge = getAge(currentPerson);
+        return oldestAge < currentAge ? currentPerson : oldestPerson;
+    });
 };
+
+function getAge(person) {
+    if (!person.yearOfDeath) {
+        let deathYear = new Date().getFullYear();
+        return deathYear - person.yearOfBirth;
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
